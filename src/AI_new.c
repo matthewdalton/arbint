b8bbc89aefe4c92d316f3fc3ed8d354eb3970c3c
Matthew Dalton <msdalton@gmail.com>
2016-05-11 23:45:46 +1000

    Division fixed for very large numbers, though performance may be an issue. Also fixed some memory leaks.
    Conflicts:
    	src/AI_new.c
    	src/arbint-test.c

--- src/AI_new.c
+++ src/AI_new.c
@@ -219,6 +219,7 @@ ArbInt *AI_NewArbInt_FromCopy(ArbInt const *ai)
   return ret;
 }
 
+#ifdef 1
 void *AI_Normalise(ArbInt *val);
 
 ArbInt *AI_NewArbInt_SetBit(aibase_t bit) /* 0-based */
@@ -246,6 +247,32 @@ ArbInt *AI_NewArbInt_SetBit(aibase_t bit) /* 0-based */
 
   return ret;
 }
+#else
+ArbInt *AI_NewArbInt_SetBit(aibase_t bit) /* 0-based */
+{
+  static const aibase_t bits_per_unit = sizeof(aibase_t) * 8;
+  aibase_t num_units = (bit / bits_per_unit);
+  aibase_t high_pos = (bit % bits_per_unit);
+
+  if (high_pos > 0)
+    ++num_units;
+
+  ArbInt *ret = ai_new_empty();
+  if (ret == NULL)
+    return NULL;
+
+  aibase_t *lv = AI_malloc(num_units * sizeof(aibase_t));
+  if (lv == NULL) {
+    AI_FreeArbInt(ret);
+    return NULL;
+  }
+
+  lv[num_units - 1] = 1 << high_pos;
+  ai_assign_value(ret, lv, num_units, 1);
+
+  return ret;
+}
+#endif
 
 ArbInt *AI_Resize(ArbInt *val, unsigned long newsize)
 {
